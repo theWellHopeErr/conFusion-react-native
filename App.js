bddb2a3ec321663e52ab6add0b72826769ee2345
theWellHopeErr <ssuryarajan@gmail.com>
2020-04-23 20:20:07 +0530

    Initial Setup

--- App.js
+++ App.js
@@ -11,6 +11,7 @@ export default function App() {
     <View style={styles.container}>
       <Text style={styles.welcome}>Welcome to React Native!</Text>
       <Text style={styles.instructions}>To get started, edit App.js</Text>
+      <Text>Hello</Text>
       <Text style={styles.instructions}>{instructions}</Text>
     </View>
   );
